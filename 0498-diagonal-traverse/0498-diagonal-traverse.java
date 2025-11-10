8e1fc3e4b5d4e2eaeb79b7da23226f34c16bf672
piyush tripathi <tripathipiyush0216@gmail.com>
2025-11-10 20:25:59 +0530

    Time: 2 ms (99.47%), Space: 47.8 MB (20.12%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,41 +1,42 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
+        int c = 0;
+        int r = 0;
         int m = mat.length;
         int n = mat[0].length;
-        
-        //  wee Use HashMap to store diagonals taki last me reverse kr ske
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int key = i + j;
-                map.putIfAbsent(key, new ArrayList<>());
-                map.get(key).add(mat[i][j]);
+        int size = m * n;
+        int dir = 1;
+        int idx = 0;
+        int[] res = new int[size];
+
+        while (idx < size) {
+            res[idx++] = mat[r][c];
+
+            if (dir == 1) {
+                if (c == n - 1) {
+                    r++;
+                    dir = -1;
+                } else if (r == 0) {
+                    c++;
+                    dir = -1;
+                } else {
+                    r--;
+                    c++;
+                }
+            } else {
+                if (r == m - 1) {
+                    c++;
+                    dir = 1;
+                } else if (c == 0) {
+                    r++;
+                    dir = 1;
+                } else {
+                    r++;
+                    c--;
+                }
             }
         }
-        
-        List<Integer> resultList = new ArrayList<>();
-        boolean flip = true;
-        
-        for (int k = 0; k <= m + n - 2; k++) {
-            List<Integer> diagonal = map.get(k);
-            if (diagonal == null) continue;
-            
-            if (flip) {
-                Collections.reverse(diagonal);
-            }
-            resultList.addAll(diagonal);
-            
-            flip = !flip;
-        }
-        
-        // Convert result list to array
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < resultList.size(); i++) {
-            result[i] = resultList.get(i);
-        }
-        
-        return result;
+
+        return res;
     }
 }
