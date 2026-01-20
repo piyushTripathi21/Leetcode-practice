1f2ce27415f3f10692d33ee50604a6c648e0be3e
piyush tripathi <tripathipiyush0216@gmail.com>
2026-01-20 21:18:28 +0530

    Time: 0 ms (100%), Space: 43 MB (60.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();  // for removing extra space we use this fxn
+
+        int i = s.length() - 1;
         int count = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                count++;
-            } else {
-                break;
-            }
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+
         return count;
     }
 }
-
-        
