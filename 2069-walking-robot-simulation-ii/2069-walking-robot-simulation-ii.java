974a5c6ed651cb8284416a83d67d78952b3433c1
piyush tripathi <tripathipiyush0216@gmail.com>
2026-04-07 21:45:52 +0530

    Time: 59 ms (93.01%), Space: 59.7 MB (6.29%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -1,64 +1,43 @@
 class Robot {
-    int width, height;
-    int x, y;
-    int dir; // 0 = East, 1 = North, 2 = West, 3 = South
-    int perimeter;
 
-    String[] directions = {"East", "North", "West", "South"};
+    int idx = 0;
+    boolean moved = false;
+    List<int[]> pos = new ArrayList<>();
 
+  
     public Robot(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.x = 0;
-        this.y = 0;
-        this.dir = 0; // East
-        this.perimeter = 2 * (width + height) - 4;
+        for (int x = 0; x < width; x++) {
+            pos.add(new int[]{x, 0, 0});
+        }
+        for (int y = 1; y < height; y++) {
+            pos.add(new int[]{width - 1, y, 1});
+        }
+        for (int x = width - 2; x >= 0; x--) {
+            pos.add(new int[]{x, height - 1, 2});
+        }
+        for (int y = height - 2; y > 0; y--) {
+            pos.add(new int[]{0, y, 3});
+        }
+        pos.get(0)[2] = 3;
     }
 
     public void step(int num) {
-        num %= perimeter;
-
-        if (num == 0) {
-            if (x == 0 && y == 0) {
-                dir = 3; // South
-                return;
-            }
-            num = perimeter;
-        }
-
-        while (num > 0) {
-            if (dir == 0) { // East
-                int move = Math.min(num, width - 1 - x);
-                x += move;
-                num -= move;
-                if (num > 0) dir = 1;
-            } 
-            else if (dir == 1) { // North
-                int move = Math.min(num, height - 1 - y);
-                y += move;
-                num -= move;
-                if (num > 0) dir = 2;
-            } 
-            else if (dir == 2) { // West
-                int move = Math.min(num, x);
-                x -= move;
-                num -= move;
-                if (num > 0) dir = 3;
-            } 
-            else { // South
-                int move = Math.min(num, y);
-                y -= move;
-                num -= move;
-                if (num > 0) dir = 0;
-            }
-        }
+        moved = true;
+        idx = (idx + num) % pos.size();
     }
 
     public int[] getPos() {
-        return new int[]{x, y};
+        return new int[]{pos.get(idx)[0], pos.get(idx)[1]};
     }
 
     public String getDir() {
-        return directions[dir];
+        if (!moved) return "East";
+
+        int d = pos.get(idx)[2];
+
+        if (d == 0) return "East";
+        else if (d == 1) return "North";
+        else if (d == 2) return "West";
+        return "South";
     }
 }
