2eadf35e3393b7fd403e92e84c8f8819b2fd3b14
piyush tripathi <tripathipiyush0216@gmail.com>
2026-07-30 18:34:39 +0530

    Time: 67 ms (35.65%), Space: 47.8 MB (43.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,45 +1,44 @@
 class Solution {
-
     public boolean canPartition(int[] nums) {
-        int sum = 0;
 
-        for (int num : nums) {
-            sum += num;
+        int n = nums.length;
+
+        int total = 0;
+
+        for(int num : nums)
+        total += num;
+
+        if(total % 2 != 0)
+        return false;
+
+        int sum = total / 2;
+
+        boolean[][] t = new boolean[n+1][sum+1];
+
+        for(int i =0; i<= n; i++){
+            for(int j = 0; j<= sum; j++){
+
+                if(i == 0)
+                t[i][j]= false;
+
+                if(j == 0)
+                t[i][j]= true;
+            }
         }
+        for(int i =1; i <= n; i++){
+            for(int j =1; j<= sum; j++){
 
-        if (sum % 2 != 0) {
-            return false;
-        }
+                if(nums[i-1] <= j){
 
-        return subsetSum(nums, sum / 2, nums.length);
-    }
+                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
 
-    boolean subsetSum(int[] arr, int sum, int n) {
+                }else{
 
-        boolean[][] t = new boolean[n + 1][sum + 1];
-
-        // Initialization
-        for (int i = 0; i <= n; i++) {
-            t[i][0] = true;
-        }
-
-        for (int j = 1; j <= sum; j++) {
-            t[0][j] = false;
-        }
-
-        // DP
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-
-                if (arr[i - 1] <= j) {
-                    t[i][j] = t[i - 1][j - arr[i - 1]]
-                           || t[i - 1][j];
-                } else {
-                    t[i][j] = t[i - 1][j];
+                    t[i][j] = t[i-1][j];
                 }
             }
         }
-
         return t[n][sum];
+        
     }
 }
